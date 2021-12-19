c75a2827840c6de2d5c001d40bf4eb932af7fdd5
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-12-19 14:01:59 -0500

    deleted API.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 const { typeDefs, resolvers } = require("./schemas");
-const db = require('./config/connection');
+const db = require ("./config/connection");
 const { authMiddleware } = require('./utils/auth');
+const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
