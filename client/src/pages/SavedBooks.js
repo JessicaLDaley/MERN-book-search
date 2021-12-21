3ac9f95c67c54d3c04f3ac07c81ebb336787e6fd
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-12-20 20:52:35 -0500

    set up full-stack structure with client side React app

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Jumbotron,Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
